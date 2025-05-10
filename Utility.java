c0d6c0f437fc50429e0856291f1b31bab2af27f7
Ady823 <aduval@brooksschool.org>
2025-05-10 13:25:20 +0000

    implement first properties

--- Utility.java
+++ Utility.java
@@ -4,6 +4,23 @@ public class Utility extends Property{
         super(name, price, location);
         
     }
-    
+
     //TODO: implement getRent method 
+    public int getRent() {
+        int rent = 0;
+        if (owner != null) {
+            int numUtilities = 0;
+            for (Property property : owner.getProperties()) {
+                if (property instanceof Utility) {
+                    numUtilities++;
+                }
+            }
+            if (numUtilities == 1) {
+                rent = 4 * owner.getLocation();
+            } else if (numUtilities == 2) {
+                rent = 10 * owner.getLocation();
+            }
+        }
+        return rent;
+    }
 }
